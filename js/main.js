f474643dc880f3bd728fe2cdb81073bf27692a7e
averinanastya99 <nastyashirokaya99@gmail.com>
2022-01-10 23:39:35 +0300

    swiper

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 new WOW().init();
-const swiper = new Swiper('.swiper', {
-    speed: 400,
-    spaceBetween: 100,
-  });
-  
+var mySwiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.projects-pagination',
+    bulletClass: 'projects-bullet'
+    bulletActiveClass: 'projects-bullet-active'
+    clickable: true;
+  },
+});
